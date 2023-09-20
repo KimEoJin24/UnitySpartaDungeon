21791e562778112da9163c907f1c04c922ec90a4
KimEoJin24 <kamejk24@gmail.com>
2023-09-20 20:46:46 +0900

    [ADD] UIManager

---
+++ UnitySpartaDungeon/Assets/Scripts/Golbal/UIManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private GameManager gameManager;
+    [SerializeField] private Button jobButton;
+    [SerializeField] private Button statusButton;
+    [SerializeField] private Button inventoryButton;
+    [SerializeField] private Button jobStoryPanelExit;
+    [SerializeField] private Button statusPanelExit;
+    [SerializeField] private Button inventoryPanelExit;
+
+
+    private void Start()
+    {
+        if(jobButton != null)
+        {
+            jobButton.onClick.AddListener(GameManager.instance.JobButton);
+        }
+
+        if(statusButton != null)
+        {
+            statusButton.onClick.AddListener(GameManager.instance.StatusButton);
+        }
+
+        if(inventoryButton != null)
+        {
+            inventoryButton.onClick.AddListener(GameManager.instance.InventoryButton);
+        }
+
+        if(jobStoryPanelExit != null)
+        {
+            jobStoryPanelExit.onClick.AddListener(GameManager.instance.JobStoryPanelExit);
+        }
+
+        if(statusPanelExit != null)
+        {
+            statusPanelExit.onClick.AddListener(GameManager.instance.StatusPanelExit);
+        }
+
+        if(inventoryPanelExit != null)
+        {
+            inventoryPanelExit.onClick.AddListener(GameManager.instance.InventoryPanelExit);
+        }
+
+    }
+}
