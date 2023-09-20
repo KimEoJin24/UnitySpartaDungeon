cd6cd21dd46c441f285ca96928368d72deeb5872
KimEoJin24 <kamejk24@gmail.com>
2023-09-20 20:46:33 +0900

    [ADD] GameManager

---
+++ UnitySpartaDungeon/Assets/Scripts/Golbal/GameManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    [SerializeField] private GameObject statusPanel;
+    [SerializeField] private GameObject inventoryPanel;
+    [SerializeField] private GameObject jobStoryPanel;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+        statusPanel.SetActive(false);
+        inventoryPanel.SetActive(false);
+        jobStoryPanel.SetActive(false);
+        
+    }
+
+    public void JobButton()
+    {
+        jobStoryPanel.SetActive(true);
+    }
+
+    public void StatusButton()
+    {
+        statusPanel.SetActive(true);
+    }
+
+    public void InventoryButton()
+    {
+        inventoryPanel.SetActive(true);
+    }
+
+    public void JobStoryPanelExit()
+    {
+        jobStoryPanel.SetActive(false);
+    }
+
+    public void StatusPanelExit()
+    {
+        statusPanel.SetActive(false);
+    }
+
+    public void InventoryPanelExit()
+    {
+        inventoryPanel.SetActive(false);
+    }
+
+    
+}
